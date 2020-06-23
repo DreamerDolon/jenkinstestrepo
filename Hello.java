7c78223b57438588a45d79212f08a4d0ef2c2c86
DreamerDolon <58173468+DreamerDolon@users.noreply.github.com>
2020-06-23 18:59:33 +0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main (String args[]) 
     {
-       System.out.println ("Hay...It's working..!!");
+       System.out.println ("Hay...It's working..!!!!!!!!!");
     }
 }
